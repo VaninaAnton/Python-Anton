d326289118a109902e7f37e33d8eedb9915c233b
Vanina Anton <vani.anton7@gmail.com>
2024-05-26 18:19:00 -0300

    cuarto commit

--- project/core/urls.py
+++ project/core/urls.py
@@ -8,5 +8,4 @@ app_name = "core"
 urlpatterns = [
     path("", index, name="index"),
     path("logout/", LogoutView.as_view(template_name="core/logout.html"), name="logout"),
-    path("sobre-nosotros/", sobre_nosotros, name= "nosotros"),
 ]
